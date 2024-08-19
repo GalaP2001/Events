7760596c90cd95befe49b1b479002140cdeb90f7
Sergej2001 <pajovicsergej07@gmail.com>
2024-08-19 14:41:59 +0200

    proba

--- src/main/java/entities/Event.java
+++ src/main/java/entities/Event.java
@@ -11,7 +11,7 @@ public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+   /// proba
     @Column(nullable = false)
     private String name;
 
